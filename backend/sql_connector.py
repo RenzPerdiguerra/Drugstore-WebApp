53c202ef5af7cdfbeeed2b1dca3659d2ed84584b
RenzPerdiguerra <perdiguerrarenzian@gmail.com>
2025-09-12 14:57:16 +0800

    Update error on connecting postgresql after test

--- backend/sql_connector.py
+++ backend/sql_connector.py
@@ -2,10 +2,11 @@ import psycopg2
 
 __cnx = None
 
-def get_connection():
+def get_sql_connection():
+    global __cnx
     if __cnx is None:
         __cnx = psycopg2.connect(dbname="mp", user="postgres",
-                                 password="your_password", host="localhost",
+                                 password="mayday09", host="localhost",
                                  port="5432")
     return __cnx
 
