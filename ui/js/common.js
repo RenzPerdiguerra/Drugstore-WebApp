8e3d0b3f21310d5c05b2933a95ab80a4173a3898
RenzPerdiguerra <perdiguerrarenzian@gmail.com>
2025-10-01 14:17:31 +0800

    Add ajax functionality from jquery for interactive development

--- ui/js/common.js
+++ ui/js/common.js
@@ -1,5 +1,15 @@
 
 var productsListApiUrl = 'http://127.0.0.1:5000/getProducts';
-var productsSaveApiUrl = 'http://127.0.0.1:5000/insertProducts';
-var productsRemoveApiUrl = 'http://127.0.0.1:5000/deleteProducts';
+var productsSaveApiUrl = 'http://127.0.0.1:5000/insertProduct';
+var productsRemoveApiUrl = 'http://127.0.0.1:5000/deleteProduct';
+
+function callApi(method, url, data) {
+    $.ajax({
+        method: method,
+        url: url,
+        data: data
+    }).done(function( msg ) {
+        window.location.reload();
+    });
+}
 
