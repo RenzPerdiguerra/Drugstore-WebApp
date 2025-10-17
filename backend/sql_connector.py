45424325f684fb98fbe1030ebe5b51b1d81339ea
RenzPerdiguerra <perdiguerrarenzian@gmail.com>
2025-10-17 18:35:51 +0800

    Modify header to remain fixed while scrolling

--- backend/sql_connector.py
+++ backend/sql_connector.py
@@ -6,8 +6,8 @@ def get_sql_connection():
     global __cnx
     if __cnx is None:
         __cnx = psycopg2.connect(dbname="mp", user="postgres",
-                                 password="mayday09", host="localhost",
-                                 port="5432")
+                                password="mayday09", host="localhost",
+                                port="5432")
     return __cnx
 
 
