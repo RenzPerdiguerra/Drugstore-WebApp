bae0da524d6c36c097fd67125d9645d58784f090
RenzPerdiguerra <perdiguerrarenzian@gmail.com>
2025-10-26 17:47:27 +0800

    Add manage-product.js button and form functions

--- ui/js/common.js
+++ ui/js/common.js
@@ -1,8 +1,9 @@
-
+// reusable APIs
 var productListApiUrl = 'http://127.0.0.1:5000/getProducts';
 var productSaveApiUrl = 'http://127.0.0.1:5000/insertProduct';
 var productRemoveApiUrl = 'http://127.0.0.1:5000/deleteProduct';
 
+// manage-product.js methods
 function callApi(method, url, data) {
     return $.ajax({
         method: method,
@@ -11,5 +12,42 @@ function callApi(method, url, data) {
         contentType: 'application/json',
         dataType: 'json'
     })
+}
+
+function showModal(formHtml) {
+    $('#modalContent').html(formHtml);
+    $('#modalOverlay').removeClass('hidden');
+}
+
+function hideModal() {
+    $('#modalOverlay').addClass('hidden');
+    $('#modalContent').empty();
+}
+
+function escapeHtml(str) {
+return String(str)
+    .replace(/&/g, '&amp;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&#39;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;');
+}
+
+function formatDateISO(dateStr) {
+    return new Date(dateStr).toISOString().slice(0, 10);
+}
+
+function formatDateLong(dateStr) {
+    const date = new Date(dateStr);
+    return new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: '2-digit'
+    }).format(date);
+}
+
+function formatToRFC1123(isoDateStr) {
+    const date = new Date(isoDateStr);
+    return date.toUTCString();
 }
 
