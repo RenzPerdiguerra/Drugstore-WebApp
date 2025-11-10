59c2c5ec80f68c49b032bf07154b71c193972d51
RenzPerdiguerra <perdiguerrarenzian@gmail.com>
2025-11-10 19:06:03 +0800

    Create function for listing checked orders towards ordering calculation from rendered order list

--- ui/js/orders.js
+++ ui/js/orders.js
@@ -5,13 +5,14 @@ import callApi,
     hideModal,
     escapeHtml,
     formatDateISO,
-    formatDateLong}
+    formatDateLong,
+    productApi}
     from './common.js'
 
 $(function() {
     $.get(orderApi.list, function (response) {
         if (response && Array.isArray(response)) {
-            let table = '';
+            var table = '';
             $.each(response, function (index, order) {
                 table += '<tr data-id="' + order.order_id + 
                     '" data-category "' + order.category +
@@ -31,7 +32,36 @@ $(function() {
                     '</td>' +
                 '</tr>'
             });
-            $("table").find('tbody').empty().html(table);
+            $("#orders-body-table").find('tbody').empty().html(table);
         }
     })
 });
+
+// function will serve to collect all checked data
+// generate a row level data to pass to order_batches
+$(document).on('change', 'input.form-check-input', function(index) {
+    const $tr = $(this).closest('tr');
+    const requestPayload = {
+        category: $tr.data('category'),
+        g_name: $tr.data('gname'),
+        b_name: $tr.data('bname'),
+        uom : $tr.data('unit'),
+        cost : $tr.data('cost')
+    };
+
+    var formHtml = `
+        <div class="col col-md-3.5 d-flex justify-content-center">${requestPayload.g_name}</div>
+        <div class="col col-md-3.5 d-flex justify-content-center">${requestPayload.b_name}</div>
+        <div class="col col-md-2 d-flex justify-content-center">
+            <input type="number" class="form-control qty-input" min="1" value="1" style="width: 30px;">
+        </div>
+        <div class="col col-md-3 d-flex justify-content-center">${requestPayload.cost}</div>
+    `;
+
+    $("div").find("#checked-rows").empty().html(formHtml);
+});
+
+$(document).on('input', '.qty-input', function(){
+    const qty = $(this).val();
+    const cost = $(this).closest('.row');
+})
