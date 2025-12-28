305d31ebe47d522281f215f19f483e6fc62edbc8
RenzPerdiguerra <perdiguerrarenzian@gmail.com>
2025-12-28 22:31:21 +0800

    Compliment api routes for created backend blueprints

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,16 +1,21 @@
 // RESTful endpoints for listing, saving, updating, and deleting products
 export const productApi = {
-    list: 'http://127.0.0.1:5000/getProducts',
-    save: 'http://127.0.0.1:5000/insertProduct',
-    update: 'http://127.0.0.1:5000/updateProduct',
-    remove: 'http://127.0.0.1:5000/deleteProduct'
+    list: 'http://127.0.0.1:5000/products/getProducts',
+    save: 'http://127.0.0.1:5000/products/insertProduct',
+    update: 'http://127.0.0.1:5000/products/updateProduct',
+    remove: 'http://127.0.0.1:5000/products/deleteProduct'
 };
 
 export const orderApi = {
-    list: 'http://127.0.0.1:5000/getOrderList',
-    save: 'http://127.0.0.1:5000/insertOrderItem',
-    update: 'http://127.0.0.1:5000/updateOrderItem',
-    remove: 'http://127.0.0.1:5000/deleteOrderItem'
+    list: 'http://127.0.0.1:5000/orders/getOrderList',
+    save: 'http://127.0.0.1:5000/orders/insertOrderItem',
+    update: 'http://127.0.0.1:5000/orders/updateOrderItem',
+    remove: 'http://127.0.0.1:5000/orders/deleteOrderItem'
+};
+
+export const authApi = {
+    register: 'http://127.0.0.1:5000/auth/register',
+    login: 'http://127.0.0.1:5000/auth/authenticate'
 };
 
 // Generic AJAX wrapper for sending JSON requests to the backend
@@ -21,5 +26,5 @@ export default function callApi(method, url, data) {
         data: data,
         contentType: 'application/json',
         dataType: 'json'
-    })
+    });
 };
