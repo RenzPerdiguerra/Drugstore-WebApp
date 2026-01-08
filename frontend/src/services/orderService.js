e2346a7ce67b441dde87ebcc6325cbddb5f39cc1
RenzPerdiguerra <perdiguerrarenzian@gmail.com>
2026-01-08 22:30:46 +0800

    Finalize order submission func and create pending_batches data

--- frontend/src/services/orderService.js
+++ frontend/src/services/orderService.js
@@ -1,4 +1,5 @@
-import callApi, { authApi, orderApi } from "../api/api";
+import callApi, { authApi, orderApi } from "../api/api.js"
+import { hideModal } from "../utils/utility.js";
 
 export function showSelectedOrdersSection() {
     $('.orders-sections .nav-item').on('click', function (e) {
@@ -58,65 +59,73 @@ export function calculateTotal() {
     })
 }
 
-export function getEmployeeName() {
-    $(document).on('click', '.confirmOrdersForm', function() {
-        const form = $(this).val().toLowerCase();
-        const data = {
-            // endUserId: form.find('emp_id'), To be included as Primary key if needed
-            endUserName: form.find('[name="name"]')
-        };
-        return data
-    });
-}
-
 // Extracts data from checked-items
 export function collectCheckedItems() {
-    return new Promise((resolve) => {
-        $(document).on('click', '.confirmOrdersForm', function() {
-            let items = [];
-            let grandTotal = 0;
-            let counter = 0;
+    let items = [];
+    let grandTotal = 0;
+    let counter = 0;
 
-            $('.checked-items .selected-item').each(function(){
-                const $row = $(this);
-                const orderBatchesPayload = {
-                    order_id: $row.data('order_id'),
-                    category: $row.data('category'),
-                    g_name: $row.find('.col-md-4').text().trim(),
-                    b_name: $row.find('.col-md-3').first().text().trim(),
-                    cost: parseFloat($row.data('cost') || 0),
-                    qty: parseInt($row.find('.qty-input').val() || 1),
-                    total_cost: calculateSubTotal($row)
-                };
-            
-                items.push(orderBatchesPayload);
-                grandTotal += orderBatchesPayload.total_cost;
-                counter += 1;
-            })
-        })
-        resolve({items, grandTotal, counter});
+    $('.checked-items .selected-item').each(function(){
+        const $row = $(this);
+        const orderBatchesPayload = {
+            order_id: $row.data('order_id'),
+            category: $row.data('category'),
+            g_name: $row.find('.col-md-4').text().trim(),
+            b_name: $row.find('.col-md-3').first().text().trim(),
+            cost: parseFloat($row.data('cost') || 0),
+            qty: parseInt($row.find('.qty-input').val() || 1),
+            total_cost: calculateSubTotal($row)
+        };
+    
+        items.push(orderBatchesPayload);
+        grandTotal += orderBatchesPayload.total_cost;
+        counter += 1;
     });
+
+    return { items, grandTotal, counter};
 }
 
-// create row-level data for pending_batches
-export async function createPendingBatchesPayload() {
-    const { items, grandTotal, counter} = await collectCheckedItems();
-    const auth_response = await callApi('GET', authApi.me); // me has parameters so I should test if this works
-    const empData = await auth_response.json();
-    // getEndUserName(); delete this
-    const orders_response = await callApi('GET', orderApi.branchName);
-    const empBranchData = await orders_response.json();
-    
-    const data = {
-        // pb_id : serial primary key
-        b_name: empData['username'],
-        requester: empBranchData['b_name'], // should I use indexing even there is only a single kvp
-        items_qty: counter,
-        total_cost: grandTotal,
-        // created_at : timestamp default current_timestamp
-        // modified_at
-    }
+export function confirmOrdersSubmission() {
+    $(document).on('click', '.confirmOrdersBtn', async function() {
+        const $form = $('#ordersSubmitForm');
+        const employeeName = $form.find('[name="name"]').val();
+        // employeeId: form.find('emp_id'), To be included as Primary key if needed
 
-    return data
+        const {grandTotal, counter} = collectCheckedItems();
+        const authResponse = await fetch(authApi.me, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('auth.jwt')},
+                contentType: 'application/json'
+                }
+            );
+
+        const empData = await authResponse.json();
+
+        const payload = {
+            // pb_id: DB default
+            b_name: empData['username'],
+            requester: employeeName, // should I use indexing even there is only a single kvp
+            items_qty: counter,
+            total_cost: grandTotal
+            // created_at: DB default
+            // modified_at: will vary depending on user modification
+        };
+
+        console.log("Pending batches payload:", payload);
+        hideModal();
+        
+        callApi('POST', orderApi.createPendingBatch, JSON.stringify(payload))
+        .fail(function(xhr, status, error) {
+            console.error("API request failed:", status, error);
+            alert('Failed to save update. Please try again.');
+        })
+        .done(function(response) {
+            if (response) {
+                alert('Update saved successfully!');
+                location.reload();
+            }
+        });
+    });
 }
 
