f02edc4c7270728d28413baff4130a9c830b3828
RenzPerdiguerra <perdiguerrarenzian@gmail.com>
2026-03-04 09:30:48 +0800

    Refactor confirm order submission and pending batch functions. Add export batch to csv function

--- frontend/src/services/orderService.js
+++ frontend/src/services/orderService.js
@@ -1,6 +1,7 @@
 import callApi, { authApi, orderApi } from "../api/api.js"
 import { hideModal } from "../utils/utility.js";
 
+// Allows selection of panel from navbar
 export function showSelectedOrdersSection() {
     $('.orders-sections .nav-item').on('click', function (e) {
         e.preventDefault();
@@ -11,7 +12,7 @@ export function showSelectedOrdersSection() {
 
         const sectionName = $(this).data('section');
 
-        $('.body-orders-list, .body-pending-confirmation, .body-confirmed-orders').addClass('d-none');
+        $('.body-orders-list, .body-pending-confirmation, .body-confirmed-batches').addClass('d-none');
 
         if (sectionName === 'order-here') {
             $('.body-orders-list').removeClass('d-none');
@@ -29,19 +30,19 @@ export function showSelectedOrdersSection() {
         if (sectionName === 'confirmed-orders') {
             $('.body-orders-list').addClass('d-none');
             $('.orders-body-checked').addClass('d-none');
-            $('.body-confirmed-orders').removeClass('d-none');
+            $('.body-confirmed-batches').removeClass('d-none');
         }
     });
 }
 
-// provides products for each checked rows
+// provides subtotal for each checked rows
 export function calculateSubTotal($row) {
     const qty = parseInt($row.find('.qty-input').val()) || 1;
     const rowCost = parseFloat($row.data('cost')) || 0;
     return qty * rowCost;
 }
 
-// Get qty input value to provide total cost per item and iterated over rendered items for the sum
+// Get qty input value to provide total cost per item and iterate over rendered items for the sum
 export function calculateTotal() {
     $(document).on('input', '.qty-input', function(){
         const $row = $(this).closest('.selected-item');
@@ -67,31 +68,32 @@ export function collectCheckedItems() {
 
     $('.checked-items .selected-item').each(function(){
         const $row = $(this);
+        const costPerOrder = calculateSubTotal($row);
         const orderBatchesPayload = {
-            order_id: $row.data('order_id'),
             category: $row.data('category'),
             g_name: $row.find('.col-md-4').text().trim(),
             b_name: $row.find('.col-md-3').first().text().trim(),
+            unit: $row.find('.col-md-1').text().trim(),
             cost: parseFloat($row.data('cost') || 0),
-            qty: parseInt($row.find('.qty-input').val() || 1),
-            total_cost: calculateSubTotal($row)
+            items_qty: parseInt($row.find('.qty-input').val() || 1),
+            total_cost: costPerOrder
         };
-    
+
         items.push(orderBatchesPayload);
-        grandTotal += orderBatchesPayload.total_cost;
+        grandTotal += costPerOrder;
         counter += 1;
     });
 
     return { items, grandTotal, counter};
 }
 
+// Get username from authorization token. Create order and pending batches
 export function confirmOrdersSubmission() {
-    $(document).on('click', '.confirmOrdersBtn', async function() {
-        const $form = $('#ordersSubmitForm');
+    $(document).on('click', '.confirm-orders-btn', async function() {
+        const $form = $('#orders-submit-form');
         const employeeName = $form.find('[name="name"]').val();
-        // employeeId: form.find('emp_id'), To be included as Primary key if needed
 
-        const {grandTotal, counter} = collectCheckedItems();
+        const {items, grandTotal, counter} = collectCheckedItems();
         const authResponse = await fetch(authApi.me, {
             method: 'GET',
             headers: {
@@ -102,20 +104,32 @@ export function confirmOrdersSubmission() {
 
         const empData = await authResponse.json();
 
-        const payload = {
-            // pb_id: DB default
-            b_name: empData['username'],
-            requester: employeeName, // should I use indexing even there is only a single kvp
+        const orderBatchPayload = items.map(item => ({
+            branch_name: empData['username'],
+            requester: employeeName,
+            ...item
+        }));
+
+        await fetch(orderApi.createOrderBatch, {
+            method: 'POST',
+            headers: {
+                contentType : 'application/json'
+            },
+            body: JSON.stringify(items)
+        });
+
+        const pendingBatchPayload = {
+            branch_name: empData['username'],
+            requester: employeeName,
             items_qty: counter,
             total_cost: grandTotal
-            // created_at: DB default
-            // modified_at: will vary depending on user modification
         };
 
-        console.log("Pending batches payload:", payload);
+        console.log("Order batches payload: ", orderBatchPayload);
+        console.log("Pending batches payload: ", pendingBatchPayload);
         hideModal();
         
-        callApi('POST', orderApi.createPendingBatch, JSON.stringify(payload))
+        callApi('POST', orderApi.createPendingBatch, JSON.stringify(pendingBatchPayload))
         .fail(function(xhr, status, error) {
             console.error("API request failed:", status, error);
             alert('Failed to save update. Please try again.');
@@ -129,3 +143,93 @@ export function confirmOrdersSubmission() {
     });
 }
 
+// Gets auth, filter role and create confirmed batch
+export function confirmPendingBatch() {
+    $(document).on('click', '#pb-confirm-btn', async function(){
+        const authResponse = await fetch(authApi.me, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('auth.jwt')},
+                contentType: 'application/json'
+                }
+            );
+        
+        const userData = await authResponse.json();
+
+        if (userData['role'] != 'admin') {
+            alert('Confirmation available for management only. Please contact respective manager.')
+            return;
+        }
+
+        confirm("Are you sure you want to confirm this pending batch?");
+        const payload = $(this).data('id');
+
+        callApi('POST', orderApi.createConfirmedBatch, JSON.stringify(payload))
+        .fail(function(xhr, status, error) {
+            console.error("API request failed:", status, error);
+            alert('Failed to save update. Please try again.');
+        })
+        .done(function(response) {
+            if (response) {
+                alert('Update saved successfully!');
+                location.reload();
+            }
+        });
+    });
+}
+
+// Renders edit form of order batch from selected pending batch
+export function editPendingBatchItems() {
+    $(document).on('click', '#pb-caret-btn', function () {
+        showModal(formHtml);
+    });
+}
+
+// Reads confirmed batch row and allows user to download csv file
+export function exportOrderBatchCsv() {
+    $(document).on('click', '#cb-export-btn', async function() {
+        const authResponse = await fetch(authApi.me, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('auth.jwt')},
+                contentType: 'application/json'
+                }
+            );
+        
+        const userData = await authResponse.json();
+
+        if (userData['role'] != 'admin') {
+            alert('Confirmation available for management only. Please contact respective manager.')
+            return;
+        }
+
+        const batchId = {
+            cb_id: $(this).data('id')
+        };
+
+        try {
+            const res = await fetch(orderApi.exportBatch, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(batchId)
+            })
+            if (!res.ok) {
+                throw new Error('Failed to fetch batch data');
+            }
+            const blob = await res.blob();
+            const url = window.URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = 'confirmed_batch.csv';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+            window.URL.revokeObjectURL(url);
+        } catch (err) {
+            console.error('Error:', err);
+            alert('Failed to export batch. Please try again.');
+        }
+    });
+}
