8aa025c3b8ef59104d837312479da924e0850103
RenzPerdiguerra <perdiguerrarenzian@gmail.com>
2026-04-03 16:13:30 +0800

    Chore(env): add VS Code Live Server listening port

--- backend/env_config.py
+++ backend/env_config.py
@@ -12,6 +12,7 @@ class DevelopmentConfig(Config):
     DEBUG        = True
     DATABASE_URL = os.environ.get('DATABASE_URL')
     CORS_ORIGINS = ['http://localhost:5500', 'http://127.0.0.1:5500',
+                    'http://localhost:3000','http://127.0.0.1:3000',
                     'http://localhost:5000', 'http://127.0.0.1:5000']
 
 class ProductionConfig(Config):
